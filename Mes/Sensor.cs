a1530a6787fdfba654069ba0499a5b7b0a6887d4
Peter Pham <p3pham@uwaterloo.ca>
2012-03-14 16:13:40 -0400

    Sensor class added based on class diagram

---
+++ Mes/Sensor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Sensor
+{
+    bool isEnabled;
+    bool isTriggered;
+    string location;
+    string sensorType;
+    bool canTrigger;
+    int sensorId;
+
+	public Sensor()
+	{
+	}
+
+    public void Enable()
+    {
+
+    }
+
+    public void Disable()
+    {
+
+    }
+
+    public void Trigger()
+    {
+
+    }
+
+    public void Untrigger()
+    {
+
+    }
+
+    public bool isEnabled()
+    {
+        return isEnabled;
+    }
+
+    public void SendEvent()
+    {
+
+    }
+
+    public void ReceiveEvent()
+    {
+
+    }
+
+}
