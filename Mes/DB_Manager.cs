40b806e7fbe9393d3e7348d7402979b5ac95c622
bk2chan <bobchan1990@hotmail.com>
2012-03-14 17:15:25 -0400

    Added database components to the project.

--- Mes/DB_Manager.cs
+++ Mes/DB_Manager.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace Mes
 {
     class DB_Manager
     {
+        SqlConnection sql_Connection = new SqlConnection(Mes.Properties.Settings.Default.MesConnectionString);
+
     }
 }
