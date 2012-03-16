93aefb197422b206e3622fd7d73f4245a899ac4d
bk2chan <bobchan1990@hotmail.com>
2012-03-16 18:02:54 -0400

    Added basic skeleton for DB_Manager class to creation the connection string and create the needed tables.
    Changed Server class to contain a Terminal class as well to run as a
    thread.

--- Mes/DB_Manager.cs
+++ Mes/DB_Manager.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
+using MySql.Data;
 
 namespace Mes
 {
     class DB_Manager
     {
-        SqlConnection sql_Connection = new SqlConnection(Mes.Properties.Settings.Default.MesConnectionString);
+        string connectionString;
 
+        public void setConnection(string _database,string _server,string _user,string _password)
+        {
+            connectionString = string.Format("Server={0};Database={1};UID={2};Password={3}",_server,_database,_user,_password);
+        }
+
+        public bool createSensorTable()
+        {
+            return false;
+        }
+
+        public bool createAlarmTable()
+        {
+            return false;
+        }
+
+        public bool createMonitorTable()
+        {
+            return false;
+        }
     }
 }
--- Mes/Server.cs
+++ Mes/Server.cs
@@ -2,22 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Mes
 {
-    class Server
-    {
-        static void Main(string[] args)
-        {
 
-            bool online = true;
+    public class Terminal
+    {
+        
+        bool online = true;
+
+        public void startTerminal()
+        {
+            online = true;
+        }
+
+        public void stopTerminal()
+        {
+            online = false;
+        }
+
+        public bool terminalState()
+        {
+            return online;
+        }
+
+        public void runTerminal()
+        {
 
             Console.WriteLine("Intializing Security Server.");
             Console.WriteLine("Welcome to Mesmerize One Prime X2");
 
             while (online)
             {
-
                 String command = Console.ReadLine();
 
                 switch (command.ToUpper())
@@ -35,11 +52,42 @@ namespace Mes
                         Console.WriteLine("Adding Sensor");
                         break;
                     case "EXIT":
-                        Console.WriteLine("Server is now shutting down...");
+                        Console.WriteLine("Command Terminal shutting down!");
+                        online = false;
                         break;
                     default:
                         Console.WriteLine("Error: Invalid command " + command + " was entered!");
                         break;
+                }
+                Thread.Sleep(0);
+            }
+        }
+    }
+
+    class Server
+    {
+
+        static void Main(string[] args)
+        {
+
+            bool running = true;
+            Terminal serverTerminal = new Terminal();
+            Thread terminalThread = new Thread(new ThreadStart(serverTerminal.runTerminal));
+
+            terminalThread.Start();
+
+            while (running)
+            {
+
+                Thread.Sleep(1000);
+
+                if (!serverTerminal.terminalState())
+                {
+                    Console.WriteLine("Server shutting down!");
+                    Thread.Sleep(2000);
+                    terminalThread.Abort();
+                    running = false;
+                    break;
                 }
             }
         }
