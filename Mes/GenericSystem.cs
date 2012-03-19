d8fc22e74997f57797425a9f4f6af11f557c1baf
unknown <bobby2@bobby2-ASUS.(none)>
2012-03-19 13:32:02 -0400

    Renamed System Interface to GenericSystem interface and applied the appropriate changes to each class.

---
+++ Mes/GenericSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mes
+{
+    public interface GenericSystem
+    {
+        //Add Stuff. Not Sure as to what to add here yet...
+        void Run();
+    }
+}
